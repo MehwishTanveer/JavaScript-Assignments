144ee597c09fa00be568ce3e0b31fdf1bb585d95
MehwishTanveer <mehwish.tanveer8@gmail.com>
2017-06-06 15:10:55 +0500

    Assignment 2

---
+++ JS_assignment2/script.js
@@ -0,0 +1,41 @@
+function var1() {
+    var userName = "Mehwish";
+    alert(userName);
+}
+
+function var2() {
+    var message = "Hello World!" ;
+    alert(message);
+}
+
+function var3() {
+    var studentName = "Mehwish";
+    var studentAge = "22";
+
+    alert (studentName);
+    alert (studentAge+" years old");
+
+    var uni = "SSUET";
+    var mod = "A";
+    alert (uni);
+    alert ("Module "+mod)
+}
+
+function var4() {
+    var email= "mehwish@gmail.com";
+    alert("My email address is "+email)
+}
+
+function var5() {
+    var geoLocation = "Srilanka";
+    var partner = "Gora";
+    var numOfKids = "12";
+    var jobTitle = "Software Developer"
+
+    alert("You will be a "+jobTitle+ " in "+geoLocation+ ", and married to a "+partner+ " with " +numOfKids+ " kids."  );
+}
+
+function var6() {
+    var text = "Yay! I can write HTML content through JavaScript.";
+    document.write(text);
+}
