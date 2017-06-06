fdf41f152ad8cd89297bfd6f446ae041a1faf02a
MehwishTanveer <mehwish.tanveer8@gmail.com>
2017-06-06 15:10:36 +0500

    Assignment 1

---
+++ JS_assignment1/script.js
@@ -0,0 +1,16 @@
+function alert1() {
+    alert ("Welcome!");
+}
+
+function alert2() {
+    alert ("Error! Please enter a valid password.");
+}
+
+function alert3() {
+    alert("Welcome to JS land...\n Happy Coding");
+}
+
+function alert4() {
+    alert("Welcome to JS Land...")
+    alert("Happy coding!")
+}
